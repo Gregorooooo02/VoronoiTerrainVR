11b395216c345ac4b679500af43ccffcdb31ac5b
Grzegorz Bednarek <gregorooooo02@gmail.com>
2026-04-14 22:38:02 +0200

    feat: Noise uses octaves and persistance to create more variation on the noise

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
@@ -6,13 +7,41 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] private int height = 256;
     [SerializeField] private float noiseScale = 20f;
 
+    [SerializeField] private int octaves = 1;
+    [Range(0, 1)]
+    [SerializeField] private float persistance = 1;
+    [SerializeField] private float lacunarity = 1;
+
+    [SerializeField] private int seed = 0;
+    [SerializeField] private Vector2 offset;
+
     [SerializeField] public bool autoUpdate = false;
 
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(width, height, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(width, height, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
         MapDisplay display = FindAnyObjectByType<MapDisplay>();
         display.DrawNoiseMap(noiseMap);
+    }
+
+    public void OnValidate()
+    {
+        if (width < 1)
+        {
+            width = 1;
+        }
+        if (height < 1)
+        {
+            height = 1;
+        }
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
     }
 }
