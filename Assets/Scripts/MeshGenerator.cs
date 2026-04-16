6571f8d3e60c1fbebce625c853383f2eddc1239b
Grzegorz Bednarek <gregorooooo02@gmail.com>
2026-04-16 23:39:54 +0200

    feat: Started working on MeshGenerator

---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public static class MeshGenerator
+{
+    public static void GenerateTerrainMesh(float[,] heightMap)
+    {
+        int width = heightMap.GetLength(0);
+        int height = heightMap.GetLength(1);
+
+        for (int y = 0; y < height; y++)
+        {
+            
+        }
+    }
+}   
